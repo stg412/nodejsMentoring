1f10ece4b3a2711f8bc30b95a47ab0772bb879b2
Maria_Korneeva <Maria_Korneeva@epam.com>
2018-05-24 15:40:47 +0300

    add reverse and transform commands

--- utils/streams.js
+++ utils/streams.js
@@ -1,4 +1,5 @@
 const parseArgs = require('minimist');
+const through = require('through2');
 
 const args = parseArgs(process.argv.slice(2), {
     string: ['action', 'file'],
@@ -20,7 +21,7 @@ function parseArguments(args) {
     for (let elem in args) {
         if (elem !== null && elem.length > 1) {
             argsArray.push(elem);
-            console.log(elem)
+            console.log('argsArray', argsArray)
         }
     }
 }
@@ -32,22 +33,57 @@ showHelp = (argsArray) => {
     if (argsArray.length > 0 && argsArray[0] === "help") {
         console.log("This is help message")
     } else
-    if (argsArray.length === 0) {
-        console.log("You need an argument");
+    if (argsArray.length === 1) {
+        console.log("Error: You need an argument");
         process.exit(-1);
     }
 }
 
-showHelp(argsArray);
+// showHelp(argsArray);
+
+reverse = () => {
+    let userInput = function write(buffer, encoding, next) {
+        this.push(buffer.toString().split("").reverse().join(""));
+        next();
+    };
+    process.stdin.pipe(through(userInput)).pipe(process.stdout);
+}
+
+transform  = () => {
+    let userInput = function write(buffer, encoding, next) {
+        this.push(buffer.toString().toUpperCase());
+        next();
+    };
+    process.stdin.pipe(through(userInput)).pipe(process.stdout);
+}
+
+outputFile  = () => {
+
+}
+
+convertFromFile = () => {
+
+}
+
+convertToFile = () => {
+
+}
 
 cmdCallFunction = () => {
     const argv = process.argv;
     for (let i = 0; i < argv.length; i++) {
+        if (argv[i] === '--action=transform') {
+            transform();
+            console.log('transform: something cool happened');
+        }
+        if (argv[i] === '--action=reverse') {
+            reverse();
+            console.log('reverse: something cool happened');
+        }
         if (argv[i] === '--action=outputFile' && argv[i+1] === '--file=dataFile.csv') {
             // some logic we need
-
-            showHelp(argv[i+1]);
-            console.log('outputFile: ');
+            showHelp(argsArray);
+            console.log('outputFile: something cool happened');
         }
         if (argv[i] === '--action=--help') {
             showHelp();
